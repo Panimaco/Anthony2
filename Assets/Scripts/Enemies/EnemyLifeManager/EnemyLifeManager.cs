5738b3da1f145d5daf38e90ff91366997a518f8e
adryuu <ads.040401@gmail.com>
2025-01-25 19:47:04 +0100

    1.7

--- Assets/Scripts/Enemies/EnemyLifeManager/EnemyLifeManager.cs
+++ Assets/Scripts/Enemies/EnemyLifeManager/EnemyLifeManager.cs
@@ -1,35 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyLifeManager : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player;
-    public List<EnemyModel> Enemies;
+    public int currentHealth;
+    public int maxHealth;
 
-    
     void Start()
     {
+        if (this.gameObject.name == "FlyingEnemy")
+        {
+            maxHealth = 1;
+            Debug.Log("El enemigo es volador");
+            currentHealth = maxHealth;
+        }
+        else if (this.gameObject.name == "UpDownEnemy") 
+        {
+            maxHealth = 1;
+            Debug.Log("El enemigo es arriba y abajo");
+            currentHealth = maxHealth;
+        }
+        else if (this.gameObject.name == "MoveStraight")
+        {
+            maxHealth = 2;
+            Debug.Log("El enemigo es rata");
+            currentHealth = maxHealth;
+        }
+
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+    
+    public void LoseEnemyLife()
     {
-        if (other.gameObject.CompareTag("Bubble"))
+        currentHealth--;
+        Debug.Log("Se ha restado vida");
+        if (currentHealth <= 0)
         {
-            EnemyLoseLife();
+            Debug.Log("Enemy is death");
+            Destroy(this.gameObject);
         }
     }
 
-    public void EnemyLoseLife()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (collision.gameObject.CompareTag("Bubble"))
+        {
+            Debug.Log("Ha habido choque");
+            LoseEnemyLife();
+        }
     }
 }

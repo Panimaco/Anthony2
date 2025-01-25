75fa526e657603b01ccef982a47de3efb95b6c78
adryuu <ads.040401@gmail.com>
2025-01-26 00:29:11 +0100

    1.3

---
+++ Assets/Adrian/Scripts/SceneManager/SceneControler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControler : MonoBehaviour
+{
+    //public int sceneIndex;
+    public string sceneName;
+    public void Start()
+    {
+        sceneName = "MainMenu";
+    }
+    public void ChangeScene(string name)
+    {
+        sceneName = name;
+        //sceneIndex = po;
+        SceneManager.LoadScene(name);
+        //StartCoroutine(LoadLevel());
+    }
+
+    /*IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(sceneIndex);
+        transitionAnim.SetTrigger("Start");
+    }*/
+}

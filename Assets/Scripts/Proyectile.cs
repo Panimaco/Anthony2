7ef3bb21aca14d724e10ac13dfa5b401306b1bf0
Alejandro Domínguez Santos <alexdomsan5@gmail.com>
2025-01-25 19:32:52 +0100

    a

--- Assets/Scripts/Proyectile.cs
+++ Assets/Scripts/Proyectile.cs
@@ -10,17 +10,30 @@ public class Proyectile : MonoBehaviour
     public float LifeTime = 2f;
     public Vector2 Direction;
 
+    [SerializeField]
+    private float _timeToDestroy = 0.1f;
+
+    private Animator _anim;
     private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.velocity = Direction * Speed;
+        _anim = GetComponent<Animator>();
         Destroy(gameObject, LifeTime);
     }
 
     private void OnCollisionEnter2D(Collision2D infoCollision)
     {
-        Destroy(gameObject);
+        if (infoCollision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        else
+        {
+            _anim.SetTrigger("Impact");
+            Destroy(gameObject, _timeToDestroy);
+        }
     }
 }

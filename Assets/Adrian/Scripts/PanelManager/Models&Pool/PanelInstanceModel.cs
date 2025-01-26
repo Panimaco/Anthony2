497beeb7811a3b556040e66950772990053588ac
adryuu <ads.040401@gmail.com>
2025-01-26 03:10:54 +0100

    push

--- Assets/Adrian/Scripts/PanelManager/Models&Pool/PanelInstanceModel.cs
+++ Assets/Adrian/Scripts/PanelManager/Models&Pool/PanelInstanceModel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelInstanceModel : MonoBehaviour
+public class PanelInstanceModel
 {
     public string PanelId;
 
